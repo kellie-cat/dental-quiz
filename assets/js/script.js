97e8fe7845deb903b374bcfe7e2f336a5280bdde
kellie-cat <>
2023-10-21 01:05:02 +0000

    Add Q and As to JavaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,59 @@
-// Quiz Q and As
-
-var quizData = [
+//Set the Q and As
+const qandas = [
   {
     question: "What is the hardest layer of tooth?",
-    answerOne: "Enamel",
-    answerTwo: "Pulp",
-    answerThree: "Dentine",
-    correct: "answer-one",
+    options: ["Enamel", "Pulp", "Dentine"],
+    correct: 0
   },
   {
     question: "Which one of these foods will cause tooth decay?",
-    answerOne: "Salt",
-    answerTwo: "Sugar",
-    answerThree: "Protein",
-    correct: "answer-two",
+    options: ["Salt", "Sugar", "Protein"],
+    correct: 1
   },
   {
-    question: "Which one of these is bad for teeth?",
-    answerOne: "Sparkling water",
-    answerTwo: "Cheese",
-    answerThree: "Sweet chilli flavoured crisps",
-    correct: "answer-three",
+    question: "Which one of these is the worst for teeth?",
+    options: ["Sparkling water", "Cheese", "Sweet chilli flavoured crisps"],
+    correct: 2
+  },
+  {
+    question: "Which one of these is safe for teeth?",
+    options: ["Tap water", "Diluting juice / squash that says - No Added Sugar", "Pure orange juice"],
+    correct: 0
+  },
+  {
+    question: "What is the biggest help to prevent tooth decay?",
+    options: ["Brushing teeth three times a day", "Changing toothpaste to a fluoride free veriety", "Reducing sugar intake from 5 times a day to once or twice a week"],
+    correct: 2
+  },
+  {
+    question: "Which one of these minerals is amazing for teeth?",
+    options: ["Phosphorous", "Fluoride", "Carbon"],
+    correct: 1
+  },
+  {
+    question: "What is the best way to get the most protection from fluoride toothpaste?",
+    options: ["Spit it out but don't rinse", "Spit it out and rinse the mouth with water", "Only use it once a day"],
+    correct: 0
+  },
+  {
+    question: "How many times a day is usually a good amount to brush with fluoride toothpaste to prevent cavities?",
+    options: ["Twice", "Once", "Five"],
+    correct: 0
+  },
+  {
+    question: "What is tooth decay?",
+    options: ["A cavity / a hole / rotten tooth / dental caries", "A sensitive tooth / sore for a few seconds when drinking sweet or hot or cold", "Bleeding gums / gum disease / bone loss / periodontal disease"],
+    correct: 0
+  },
+  {
+    question: "What is the best treatment for bleeding gums?",
+    options: ["Antibiotics", "Leaving them alone until they heal", "Gentle but thorough cleaning that removes all the plaque, regular professional cleaning if needed"],
+    correct: 2
   },
 ];
 
-//Collect all the elements
-const question = document.getElementById("question");
-const answerOne = document.getElementById("answer-one");
-const answerTwo = document.getElementById("answer-two");
-const answerThree = document.getElementById("answer-three");
-const submitButton = document.getElementById("submit-button");
-const allAnswer = document.querySelectorAll(".answer");
-
-//Define the quiz logic
-let score = 0;
-let i = 0;
-
-//Get the User Answer
-function startQuiz() {
-  console.log('Start quiz?');
+function pleaseHelp() {
+  console.log('Am I connected?');
 }
 
-startQuiz();
+pleaseHelp();
